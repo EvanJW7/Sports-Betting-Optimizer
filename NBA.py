1563dac4bb7095a679a0de396a6a03cc20dc2be4
Evan Wright <84414002+EvanJW7@users.noreply.github.com>
2022-01-07 16:41:30 -0600

    Update NBA.py

--- NBA.py
+++ NBA.py
@@ -44,6 +44,8 @@ def game_projection(spread, total, percent_of_bets, favteam, underdog):
         print(f"Bet the moneline on the {favteam.title()}")
     elif bet>=4:
         print(f"Bet the spread on the {favteam.title()}")
+    print('\n')
+    print('Projected box score: ')
     start = 0
     f1 = x/4  
     f2 = x/4+ x/4
@@ -79,160 +81,59 @@ def game_projection(spread, total, percent_of_bets, favteam, underdog):
     
     import matplotlib.pyplot as plt
    
+
     Quarter = ['0','12','Halftime','36', '48']
     Points1 = favteam_points
     Points2 = underdog_points
-    def getcolor(favteam):
-        if favteam == 'bucks':
-            colors = 'forestgreen'
-        if favteam == '76ers':
-            colors = 'royalblue'
-        if favteam == 'bulls':
-            colors = 'tab:red'
-        if favteam == 'cavaliers':
-            colors = 'tab:brown'
-        if favteam == 'celtics':
-            colors = 'green'
-        if favteam == 'clippers':
-            colors = 'mediumblue'
-        if favteam == 'grizzlies':
-            colors = 'lightskyblue'
-        if favteam == 'hawks':
-            colors = 'firebrick'
-        if favteam == 'heat':
-            colors = 'maroon'
-        if favteam == 'hornets':
-            colors = 'cyan'
-        if favteam == 'jazz':
-            colors = 'orange'
-        if favteam == 'kings':
-            colors = 'tab:purple'
-        if favteam == 'knicks':
-            colors = 'tab:orange'
-        if favteam == 'lakers':
-            colors = 'gold'
-        if favteam == 'magic':
-            colors = 'blue'
-        if favteam == 'mavericks':
-            colors = 'dodgerblue'
-        if favteam == 'nets':
-            colors = 'black'
-        if favteam == 'nuggets':
-            colors = 'midnightblue'
-        if favteam == 'pacers':
-            colors = 'yellow'
-        if favteam == 'pelicans':
-            colors = 'orangered'
-        if favteam == 'pistons':
-            colors = 'red'
-        if favteam == 'raptors':
-            colors = 'brown'
-        if favteam == 'rockets':
-            colors = 'crimson'
-        if favteam == 'spurs':
-            colors = 'gray'
-        if favteam == 'suns':
-            colors = 'darkorange'
-        if favteam == 'thunder':
-            colors = 'royalblue'
-        if favteam == 'timberwolves':
-            colors = 'lime'
-        if favteam == 'blazers':
-            colors = 'tomato'
-        if favteam == 'trail blazers':
-            colors = 'tomato'
-        if favteam == 'warriors':
-            colors = 'yellow'
-        if favteam == 'wizards':
-            colors = 'lightcoral'
-        return colors
-    colors = getcolor(favteam)
-    def getcolor(underdog):
-        if underdog == 'bucks':
-            coloru = 'forestgreen'
-        if underdog == '76ers':
-            coloru = 'royalblue'
-        if underdog == 'bulls':
-            coloru = 'tab:red'
-        if underdog == 'cavaliers':
-            coloru = 'tab:brown'
-        if underdog == 'celtics':
-            coloru = 'green'
-        if underdog == 'clippers':
-            coloru = 'mediumblue'
-        if underdog == 'grizzlies':
-            coloru = 'lightskyblue'
-        if underdog == 'hawks':
-            coloru = 'firebrick'
-        if underdog == 'heat':
-            coloru = 'maroon'
-        if underdog == 'hornets':
-            coloru = 'cyan'
-        if underdog == 'jazz':
-            coloru = 'orange'
-        if underdog == 'kings':
-            coloru = 'tab:purple'
-        if underdog == 'knicks':
-            coloru = 'tab:orange'
-        if underdog == 'lakers':
-            coloru = 'gold'
-        if underdog == 'magic':
-            coloru = 'blue'
-        if underdog == 'mavericks':
-            coloru = 'dodgerblue'
-        if underdog == 'nets':
-            coloru = 'black'
-        if underdog == 'nuggets':
-            coloru = 'midnightblue'
-        if underdog == 'pacers':
-            coloru = 'yellow'
-        if underdog == 'pelicans':
-            coloru = 'orangered'
-        if underdog == 'pistons':
-            coloru = 'red'
-        if underdog == 'raptors':
-            coloru = 'brown'
-        if underdog == 'rockets':
-            coloru = 'crimson'
-        if underdog == 'spurs':
-            coloru = 'gray'
-        if underdog == 'suns':
-            coloru = 'darkorange'
-        if underdog == 'thunder':
-            coloru = 'royalblue'
-        if underdog == 'timberwolves':
-            coloru = 'lime'
-        if underdog == 'blazers':
-            coloru = 'tomato'
-        if underdog == 'trail blazers':
-            coloru = 'tomato'
-        if underdog == 'warriors':
-            coloru = 'yellow'
-        if underdog == 'wizards':
-            coloru = 'lightcoral'
-        return coloru
-    coloru = getcolor(underdog)
     
-    plt.plot(Quarter, Points1, label = favteam.title(), color = getcolor(favteam), lw = 1, marker = '.', ms = 10)
-    plt.plot(Quarter, Points2, label = underdog.title(), color = getcolor(underdog), lw = 1, marker = '.', ms = 10)
+    colors = {
+        'bucks': 'forestgreen',
+        '76ers': 'royalblue',
+        'bulls': 'tab:red',
+        'cavaliers': 'tab:brown',
+        'cavs': 'tab:brown',
+        'celtics': 'green',
+        'clippers': 'mediumblue',
+        'grizzlies': 'lightskyblue',
+        'hawks': 'firebrick',
+        'heat': 'maroon',
+        'hornets': 'cyan',
+        'jazz': 'orange',
+        'kings': 'tab:purple',
+        'knicks': 'tab:orange',
+        'lakers': 'gold',
+        'magic': 'blue',
+        'mavericks': 'dodgerblue',
+        'mavs': 'dodgerblue',
+        'nets': 'black',
+        'nuggets': 'midnightblue',
+        'pacers': 'yellow',
+        'pelicans': 'orangered',
+        'pistons': 'red',
+        'raptors': 'brown',
+        'rockets': 'crimson',
+        'spurs': 'gray',
+        'suns': 'darkorange',
+        'thunder': 'royalblue',
+        'timberwolves': 'lime',
+        'wolves': 'lime',
+        'trail blazers': 'tomato',
+        'blazers': 'tomato',
+        'warriors': 'yellow',
+        'wizards': 'lightcoral'
+    }
+    
+    plt.plot(Quarter, Points1, label = favteam.title(), color = colors[favteam], lw = 1, marker = '.', ms = 10)
+    plt.plot(Quarter, Points2, label = underdog.title(), color = colors[underdog], lw = 1, marker = '.', ms = 10)
+    plt.style.use('fivethirtyeight')
     plt.title('Game Spread Projection')
     plt.xlabel('Quarter (in minutes)')
     plt.ylabel('Expected Points')
-    plt.legend(fontsize = 'large', loc=9)
-    plt.style.use('fivethirtyeight')
+    plt.legend(fontsize = 'small', loc=9)
+    
     print(data)
     plt.show()
-    #print(f'\n{favteam.title()} slope: ')
 
-    #f4 = float(f4)
-    #favteamslope = f4/48
-    #print(format(favteamslope, ".3"))
-    #print(f"\n{underdog.title()} slope: ")
-    #u4 = float(u4)
-    #underdogslope = u4/48
-    #print(format(underdogslope, ".3"))
-    #print(f"\nHalftime score: {f2} to {u2}")
-    #print(f"\nFinal score: {f4} to {u4}")
 a = float(input("Spread: "))
 b = float(input("Over/Under: "))
 c = float(input(f"Percent of bets on the favorite: "))
@@ -241,3 +142,5 @@ d = d.lower()
 e = input("Underdog: ")
 e = e.lower()
 game_projection(a, b, c, d, e)
+
+
